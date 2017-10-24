a4cbfa65975ce78f450466415116413167f5f148
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2017-10-24 10:22:02 +0200

    sokszog.py Adapt to new teki.py

--- sokszog.py
+++ sokszog.py
@@ -1,11 +1,12 @@
 from teki import *
 
+lali = Turtle()
 
-def sokszog(oldalszam, oldalhossz):
+def sokszog(oldalszam, oldalhossz, teki=lali):
     for i in range(oldalszam):
-        forward(oldalhossz)
-        right(360/oldalszam)
+        teki.forward(oldalhossz)
+        teki.right(360/oldalszam)
 
 
-def negyzet(oldalhossz):
-    sokszog(4, oldalhossz)
+def negyzet(oldalhossz, teki=lali):
+    sokszog(4, oldalhossz, teki=teki)
